e4d68c1294df9b88e8e6a143c6346b23c99f5a1c
Wisnu Alfian Nur Ashar <wisnualfian117@gmail.com>
2026-06-08 14:57:37 +0700

    [mobile] feat: implement Dark Mode toggle support in App container

--- apps/ionic-mobile/src/App.vue
+++ apps/ionic-mobile/src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <ion-app>
+  <ion-app :class="{ 'dark': isDarkMode }">
     <ion-router-outlet />
   </ion-app>
 </template>
 
 <script setup lang="ts">
 import { IonApp, IonRouterOutlet } from '@ionic/vue'
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
 import { useAuthStore } from '@/stores/auth'
 
 const authStore = useAuthStore()
+const isDarkMode = ref(false)
 
 onMounted(() => {
   authStore.initialize()
