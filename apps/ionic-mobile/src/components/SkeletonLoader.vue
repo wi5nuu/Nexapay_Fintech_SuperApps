166b546610529e9037b79aef16017ab7440c77c7
Wisnu Alfian Nur Ashar <wisnualfian117@gmail.com>
2026-06-08 14:58:59 +0700

    [mobile] ui: add Skeleton Loaders for better UX on tab pages

---
+++ apps/ionic-mobile/src/components/SkeletonLoader.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="skeleton-wrapper">
+    <ion-skeleton-text animated style="width: 100%; height: 20px; margin-bottom: 8px;"></ion-skeleton-text>
+    <ion-skeleton-text animated style="width: 80%; height: 20px;"></ion-skeleton-text>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { IonSkeletonText } from '@ionic/vue';
+</script>
