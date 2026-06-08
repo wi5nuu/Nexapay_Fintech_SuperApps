3a07b5a98a8c4387ddffef794d1b6f9d5a804b01
Wisnu Alfian Nur Ashar <wisnualfian117@gmail.com>
2026-06-08 14:56:19 +0700

    [mobile] feat: add Profile Settings page with avatar upload simulation

---
+++ apps/ionic-mobile/src/pages/ProfileSettings.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="profile-settings">
+    <h3>Profile Settings</h3>
+    <ion-item>
+      <ion-label position="stacked">Name</ion-label>
+      <ion-input v-model="name"></ion-input>
+    </ion-item>
+    <ion-button expand="block" class="ion-margin-top">Save Changes</ion-button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import { IonItem, IonLabel, IonInput, IonButton } from '@ionic/vue';
+const name = ref('Nexa User');
+</script>
