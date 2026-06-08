f9e159aaee6ef8dfe1885ed5a3f4d34828ed76a8
Wisnu Alfian Nur Ashar <wisnualfian117@gmail.com>
2026-06-08 14:53:52 +0700

    [mobile] feat: add Contact Selection feature for transfers

---
+++ apps/ionic-mobile/src/components/ContactSelectionWidget.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="contact-selection">
+    <h3>Select Contact</h3>
+    <ion-list>
+      <ion-item v-for="contact in contacts" :key="contact.id" button @click="$emit('select', contact)">
+        <ion-avatar slot="start">
+          <img :src="contact.avatar" />
+        </ion-avatar>
+        <ion-label>{{ contact.name }}</ion-label>
+      </ion-item>
+    </ion-list>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { IonList, IonItem, IonLabel, IonAvatar } from '@ionic/vue';
+
+const contacts = [
+  { id: 1, name: 'Alice', avatar: 'https://ionicframework.com/docs/img/demos/avatar.svg' },
+  { id: 2, name: 'Bob', avatar: 'https://ionicframework.com/docs/img/demos/avatar.svg' },
+];
+defineEmits(['select']);
+</script>
