f2a9951b36a80ee4f077efdfde36792b8536c0e1
Wisnu Alfian Nur Ashar <wisnualfian117@gmail.com>
2026-06-08 14:54:36 +0700

    [mobile] feat: implement Top Up options UI

---
+++ apps/ionic-mobile/src/components/TopUpWidget.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="topup-options">
+    <h3>Top Up</h3>
+    <ion-list>
+      <ion-item button>
+        <ion-label>Bank Transfer</ion-label>
+      </ion-item>
+      <ion-item button>
+        <ion-label>Credit Card</ion-label>
+      </ion-item>
+      <ion-item button>
+        <ion-label>E-Wallet</ion-label>
+      </ion-item>
+    </ion-list>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { IonList, IonItem, IonLabel } from '@ionic/vue';
+</script>
